ddd58764ba81104c494eb55611531e6cd9007500
davidleacock <david.leacock@innovapost.com>
2024-01-17 17:26:40 -0800

    create service test spec and inmemory repo

---
+++ dds/src/main/scala/repository/InMemoryDeliveryDateRepository.scala
@@ -0,0 +1,24 @@
+package repository
+
+import cats.effect.IO
+import domain.DeliveryDate
+
+import java.util.UUID
+import scala.collection.mutable
+
+class InMemoryDeliveryDateRepository extends DeliveryDateRepository {
+
+  private val memory = mutable.Map.empty[UUID, DeliveryDate]
+
+  override def getDeliveryDate(packageId: UUID): IO[Option[DeliveryDate]] = {
+    IO {
+      memory.get(packageId)
+    }
+  }
+
+  override def updateDeliveryDate(deliveryDate: DeliveryDate): IO[Unit] = {
+    IO {
+      memory.updated(deliveryDate.packageId, deliveryDate)
+    }
+  }
+}
---
+++ dds/src/test/scala/service/DeliveryDateServiceSpec.scala
@@ -0,0 +1,23 @@
+package service
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class DeliveryDateServiceSpec extends AnyWordSpec with Matchers {
+
+
+
+  "DeliveryDateService" should {
+
+    "update delivery date for a valid package" in {}
+
+    "handle non-existing package IDs by creating a new instance" in {}
+
+    "handle a failed attempt at updating delivery date" in {}
+
+    "retrieve delivery date for existing package" in {}
+
+    "return nothing when package does not exist" in {}
+
+  }
+}
